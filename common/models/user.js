882d5efbfac8fa58b7d0fea3fe8fdc21fc18ff3f
Firdous <firdous.ali86@gmail.com>
2017-11-20 17:01:18 -0800

    user

--- common/models/user.js
+++ common/models/user.js
@@ -1,5 +1,17 @@
-'use strict';
+"use strict";
 
 module.exports = function(User) {
+  User.greet = function(msg, cb) {
+    cb(null, "Greetings... " + msg);
+  };
 
+  User.remoteMethod("greet", {
+    accepts: { arg: "msg", type: "string" },
+    returns: { arg: "greeting", type: "string" }
+  });
+
+  User.beforeRemote("**", function(ctx, user, next) {
+    console.log(ctx.methodString, "was invoked remotely"); // customers.prototype.save was invoked remotely
+    next();
+  });
 };
